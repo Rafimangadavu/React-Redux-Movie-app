4c6f628c5677e4e028919cfd1ec0d2b60617b7b9
RafiMangadavu <rafimangadavu2025@gmail.com>
2022-10-09 12:58:48 +0530

    first part completede

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,71 +1,81 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import "../../styles/MovieDetails.css";
-import { fetchAsyncMovieOrShowDetail, getASelectMovieOrShow,  } from '../../features/movies/movieSlice';
-import "../../styles/MovieDetails.css"
+import '../../styles/MovieDetails.css';
+import {
+  fetchAsyncMovieOrShowDetail,
+  getASelectMovieOrShow,
+  removeSelectedMovieOrShow,
+} from '../../features/movies/movieSlice';
+import '../../styles/MovieDetails.css';
 
 const MovieDetails = () => {
-  const {imdbID} = useParams();
+  const { imdbID } = useParams();
   const dispatch = useDispatch();
   const data = useSelector(getASelectMovieOrShow);
 
   console.log(data);
 
-  useEffect(()=>{
-    dispatch(fetchAsyncMovieOrShowDetail(imdbID))
-    
-  },[dispatch, imdbID]);
+  useEffect(() => {
+    dispatch(fetchAsyncMovieOrShowDetail(imdbID));
+    return () => {
+      dispatch(removeSelectedMovieOrShow());
+    };
+  }, [dispatch, imdbID]);
 
   return (
-    <div className='movie-section'>
-      <div className="section-left">
-        <div className="movie-title">
-          {data.Title}
-        </div>
-        <div className="movie-rating">
-          <span>
-            IMDB Rating <i className='fa fa-star'></i>: {data.imdbRating}
-          </span>
-          <span>
-            IMDB Votes <i className='fa fa-thumps-up'></i>: {data.imdbVotes}
-          </span>
-          <span>
-            Runtime <i className='fa fa-film'></i>: {data.Runtime}
-          </span>
-          <span>
-            Year <i className='fa fa-calendar'></i>: {data.Year}
-          </span>
-        </div>
-        <div className="movie-plot">{data.Plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+    <div className="movie-section">
+      {Object.keys(data).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <>
+          <div className="section-left">
+            <div className="movie-title">{data.Title}</div>
+            <div className="movie-rating">
+              <span>
+                IMDB Rating <i className="fa fa-star"></i>: {data.imdbRating}
+              </span>
+              <span>
+                IMDB Votes <i className="fa fa-thumbs-up"></i>: {data.imdbVotes}
+              </span>
+              <span>
+                Runtime <i className="fa fa-film"></i>: {data.Runtime}
+              </span>
+              <span>
+                Year <i className="fa fa-calendar"></i>: {data.Year}
+              </span>
+            </div>
+            <div className="movie-plot">{data.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span className="head">Director</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span className="head">Stars</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span className="head">Generes</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span className="head">Languages</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span className="head">Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars</span>
-            <span>{data.Actors}</span>
+          <div className="section-right">
+            <img src={data.Poster} alt={data.Title} />
           </div>
-          <div>
-            <span>Generes</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Languages</span>
-            <span>{data.anguage}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={data.Poster} alt={data.Title} />
-      </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MovieDetails
+export default MovieDetails;
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -39,8 +39,8 @@ const movieSlice = createSlice({
     name: "movies",
     initialState,
     reducers:{
-        addMovies: (state, {payload}) =>{
-            state.movies = payload;
+        removeSelectedMovieOrShow: (state) => {
+            state.selectMovieOrShow = {};
         }
     },
     extraReducers: {
@@ -68,7 +68,7 @@ const movieSlice = createSlice({
 
 })
 
-export const {addMovies} = movieSlice.actions;
+export const {removeSelectedMovieOrShow} = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
 export const getAllShows = (state) => state.movies.shows;
 export const getASelectMovieOrShow = (state) => state.movies.selectMovieOrShow;
